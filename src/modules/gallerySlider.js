21fe4d282406aca92dcefeb47ba39e317891bb53
@vitaliylukin91 <vitaliylukin91@gmail.com>
2020-05-15 11:56:38 +0300

    fix gallery slider

--- src/modules/gallerySlider.js
+++ src/modules/gallerySlider.js
@@ -2,10 +2,10 @@ const gallerySlider = () => {
 
     const slider =  document.querySelector('.gallery-slider'),
         slide = slider.querySelectorAll('.slide'),
-        btn = slider.querySelectorAll('.gallery-slider > .slider-arrow'),
         dot = slider.querySelectorAll('.dot');
 
-    let currentSlide = 0;
+    let currentSlide = 0,
+        interval;
 
     const prevSlide = (elem, index, strClass) => {
         elem[index].classList.remove(strClass);
@@ -30,21 +30,66 @@ const gallerySlider = () => {
 
     };
 
-    const startSlide = (time) => {
-        setInterval(autoPlaySlide, time);
+    const startSlide = (time = 3000) => {
+        interval = setInterval(autoPlaySlide, time);
     };
 
     const stopSlide = () => {
-
+        clearInterval(interval);
     };
 
     slider.addEventListener('click', (event) => {
+        event.preventDefault();
 
+        let target = event.target;
 
+        if (!target.matches('.slider-arrow span, .dot')) {
+            return;
+        }
+
+        prevSlide(slide, currentSlide, 'active');
+        prevSlide(dot, currentSlide, 'active');
+
+        if (target.matches('#arrow-right span')) {
+            currentSlide++;
+        } else if (target.matches('#arrow-left span')) {
+            currentSlide--;
+        } else if (target.matches('.dot')) {
+            dot.forEach((elem, index) => {
+                if (elem === target) {
+                    currentSlide = index;
+                }
+            });
+        }
+
+        if (currentSlide >= slide.length) {
+            currentSlide = 0;
+        }
+
+        if (currentSlide < 0) {
+            currentSlide = slide.length - 1;
+        }
+
+        nextSlide(slide, currentSlide, 'active');
+        nextSlide(dot, currentSlide, 'active');
 
     });
+    slider.addEventListener('mouseover',(event) => {
+        let target = event.target;
+        if (target.matches('.slider-arrow span') || target.matches('.dot')) {
+            stopSlide();
+        }
+    });
 
-    startSlide(150000);
+    //Убрать мышку со стрелки или точки
+    slider.addEventListener('mouseout',(event) => {
+        let target = event.target;
+        if (target.matches('.slider-arrow span') || target.matches('.dot')) {
+            startSlide();
+        }
+    });
+
+    startSlide(3000);
 
 };
 
